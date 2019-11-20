e88ec6ad137d80df2fb0d7d5d783a819e16e0927
Vanessa Randall <vanessalrandall@gmail.com>
2019-11-20 14:16:59 -0700

    Clean songs data to eliminate unnecessary information

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const url = require('url');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+  console.log(
+    `${app.locals.title} is running on http://localhost:${app.get('port')}`
+  );
+});
