8b168ff1eb979e8432990fc448dff78a24208ae6
Vanessa Randall <vanessalrandall@gmail.com>
2019-11-22 10:30:40 -0700

    re-install npm

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,18 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true
+  },
+  production: {
+    client: 'pg'
   }
 };
+
+
+production: {
+  client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+      migrations: {
+    directory: './db/migrations'
+  },
+  useNullAsDefault: true
+}
