320ec35df7eb5e57c62efba91a1e549124863ced
Vanessa Randall <vanessalrandall@gmail.com>
2019-11-24 17:47:53 -0700

    Add albums and albums/:id GET endpoints

--- server.js
+++ server.js
@@ -10,6 +10,34 @@ app.use(express.json());
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'The Beatles api';
 
+app.get('/api/v1/albums', (request, response) => {
+  database('albums')
+    .select()
+    .then(albums => {
+      response.status(200).json(albums);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/albums/:id', (request, response) => {
+  const { id } = request.params;
+  database('albums')
+    .where({ albumId: id })
+    .then(album => {
+      if (album.length === 0) {
+        response
+          .status(404)
+          .json({ error: `There is not an album with the id of ${id}` });
+      }
+      response.status(200).json(album[0]);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
 app.listen(app.get('port'), () => {
   console.log(
     `${app.locals.title} is running on http://localhost:${app.get('port')}`
